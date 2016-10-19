9b19eb9cd0a0e07580847c4618d804d0d5b873a2
David Kirstein <dak@batix.com>
2016-10-19 19:24:52 +0200

    update permissions, attempt #2

--- docker-run-tests.sh
+++ docker-run-tests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 docker-compose -f dockercomposefile-debian8.yaml rm -fv
 docker-compose -f dockercomposefile-debian8.yaml pull && \
 docker-compose -f dockercomposefile-debian8.yaml build && \
